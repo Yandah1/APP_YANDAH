5e8a3b7ecf498ac1dd078513c6a4c764bf182bb2
Nonkululeko <132392824+Yandah1@users.noreply.github.com>
2023-08-23 01:55:22 +0200

    Update builtins.c

--- simple_shell-main/builtins.c
+++ simple_shell-main/builtins.c
@@ -2,10 +2,10 @@
 
 /**
  * check_for_builtins - checks if the command is a builtin
- * @vars: variables
+ * @Shell_vars:  shell variables
  * Return: pointer to the function or NULL
  */
-void (*check_for_builtins(vars_t *vars))(vars_t *vars)
+void (*check_for_builtins(vars_t *shell_vars))(vars_t *shell_vars)
 {
 	unsigned int i;
 	builtins_t check[] = {
@@ -18,60 +18,60 @@ void (*check_for_builtins(vars_t *vars))(vars_t *vars)
 
 	for (i = 0; check[i].f != NULL; i++)
 	{
-		if (_strcmpr(vars->av[0], check[i].name) == 0)
+		if (_strcmpr(shell_vars->av[0], check[i].name) == 0)
 			break;
 	}
 	if (check[i].f != NULL)
-		check[i].f(vars);
+		check[i].f(shell_vars);
 	return (check[i].f);
 }
 
 /**
  * new_exit - exit program
- * @vars: variables
+ * @shell_vars: variables
  * Return: void
  */
-void new_exit(vars_t *vars)
+void new_exit(vars_t *shell_vars)
 {
 	int status;
 
-	if (_strcmpr(vars->av[0], "exit") == 0 && vars->av[1] != NULL)
+	if (_strcmpr(shell_vars->av[0], "exit") == 0 && shell_vars->av[1] != NULL)
 	{
-		status = _atoi(vars->av[1]);
+		status = _atoi(shell_vars->av[1]);
 		if (status == -1)
 		{
-			vars->status = 2;
-			print_error(vars, ": Illegal number: ");
-			_puts2(vars->av[1]);
+			shell_vars->status = 2;
+			print_error(shell_vars, ": Illegal number: ");
+			_puts2(shell_vars->av[1]);
 			_puts2("\n");
-			free(vars->commands);
-			vars->commands = NULL;
+			free(shell_vars->commands);
+			shell_vars->commands = NULL;
 			return;
 		}
-		vars->status = status;
+		shell_vars->status = status;
 	}
-	free(vars->buffer);
-	free(vars->av);
-	free(vars->commands);
-	free_env(vars->env);
-	exit(vars->status);
+	free(shell_vars->buffer);
+	frees(shell_vars->av);
+	free(shell_vars->commands);
+	free_env(shell_vars->env);
+	exit(shell_vars->status);
 }
 
 /**
  * _env - prints the current environment
- * @vars: struct of variables
+ * @shell_vars: struct of variables
  * Return: void.
  */
-void _env(vars_t *vars)
+void _env(vars_t *shell_vars)
 {
 	unsigned int i;
 
-	for (i = 0; vars->env[i]; i++)
+	for (i = 0; shell_vars->env[i]; i++)
 	{
-		_puts(vars->env[i]);
+		_puts(shell_vars->env[i]);
 		_puts("\n");
 	}
-	vars->status = 0;
+	shell_vars->status = 0;
 }
 
 /**
@@ -80,78 +80,78 @@ void _env(vars_t *vars)
  *
  * Return: void
  */
-void new_setenv(vars_t *vars)
+void new_setenv(vars_t *shell_vars)
 {
 	char **key;
-	char *var;
+	char *shell_var;
 
-	if (vars->av[1] == NULL || vars->av[2] == NULL)
+	if (shell_vars->av[1] == NULL || shell_vars->av[2] == NULL)
 	{
-		print_error(vars, ": Incorrect number of arguments\n");
-		vars->status = 2;
+		print_error(shell_vars, ": Incorrect number of arguments\n");
+		shell_vars->status = 2;
 		return;
 	}
-	key = find_key(vars->env, vars->av[1]);
+	key = find_key(shell_vars->env, shell_vars->av[1]);
 	if (key == NULL)
-		add_key(vars);
+		add_key(shell_vars);
 	else
 	{
-		var = add_value(vars->av[1], vars->av[2]);
-		if (var == NULL)
+		var = add_value(shell_vars->av[1], shell_vars->av[2]);
+		if (shell_var == NULL)
 		{
-			print_error(vars, NULL);
-			free(vars->buffer);
-			free(vars->commands);
-			free(vars->av);
-			free_env(vars->env);
+			print_error(shell_vars, NULL);
+			free(shell_vars->buffer);
+			free(shell_vars->commands);
+			free(shell_vars->av);
+			free_env(shell_vars->env);
 			exit(127);
 		}
 		free(*key);
 		*key = var;
 	}
-	vars->status = 0;
+	shell_vars->status = 0;
 }
 
 /**
- * new_unsetenv - remove an environment variable
- * @vars: pointer to a struct of variables
+ * new_unsetenv - function that remove an environment variable
+ * @shell_vars: pointer to a struct of variables
  *
  * Return: void
  */
-void new_unsetenv(vars_t *vars)
+void new_unsetenv(vars_t *shell_vars)
 {
 	char **key, **newenv;
 
 	unsigned int i, j;
 
-	if (vars->av[1] == NULL)
+	if (shell_vars->av[1] == NULL)
 	{
-		print_error(vars, ": Incorrect number of arguments\n");
-		vars->status = 2;
+		print_error(shell_vars, ": Incorrect number of arguments\n");
+		shell_vars->status = 2;
 		return;
 	}
-	key = find_key(vars->env, vars->av[1]);
+	key = find_key(shell_vars->env, shell_vars->av[1]);
 	if (key == NULL)
 	{
-		print_error(vars, ": No variable to unset");
+		print_error(shell_vars, ": No variable to unset");
 		return;
 	}
-	for (i = 0; vars->env[i] != NULL; i++)
+	for (i = 0; shell_vars->env[i] != NULL; i++)
 		;
 	newenv = malloc(sizeof(char *) * i);
 	if (newenv == NULL)
 	{
-		print_error(vars, NULL);
-		vars->status = 127;
-		new_exit(vars);
+		print_error(shell_vars, NULL);
+		shell_vars->status = 127;
+		new_exit(shell_vars);
 	}
-	for (i = 0; vars->env[i] != *key; i++)
-		newenv[i] = vars->env[i];
-	for (j = i + 1; vars->env[j] != NULL; j++, i++)
-		newenv[i] = vars->env[j];
+	for (i = 0; shell_vars->env[i] != *key; i++)
+		newenv[i] = shell_vars->env[i];
+	for (j = i + 1; shell_vars->env[j] != NULL; j++, i++)
+		newenv[i] = shell_vars->env[j];
 	newenv[i] = NULL;
 	free(*key);
-	free(vars->env);
-	vars->env = newenv;
-	vars->status = 0;
+	free(shell_vars->env);
+	shell_vars->env = newenv;
+	shell_vars->status = 0;
 }
