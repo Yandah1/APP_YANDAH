e2f0e3b9094fb0402e84b2ee1b1c25c05b16174d
Nonkululeko <132392824+Yandah1@users.noreply.github.com>
2023-08-23 02:22:15 +0200

    Update shell.h

--- simple_shell-main/shell.h
+++ simple_shell-main/shell.h
@@ -3,13 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <signal.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
-#include <limits.h>
-#include <signal.h>
 
 
 /**
@@ -31,7 +31,7 @@ typedef struct variables
 	char **argv;
 	int status;
 	char **commands;
-} vars_t;
+} shell_vars_t;
 
 /**
  * struct builtins - struct for the builtin functions
@@ -53,28 +53,29 @@ int _strcmpr(char *strcmp1, char *strcmp2);
 char *_strcat(char *strc1, char *strc2);
 unsigned int _strlen(char *str);
 
+/*BUILINS*/
+void (*check_for_builtins(vars_t *shell_vars))(vars_t shell_*vars);
+void new_exit(shell_vars_t *shell_vars);
+void _env(vars_t *shell_vars);
+void new_setenv(vars_t *shell_vars);
+void new_unsetenv(vars_t *shell_vars);
+
 char **tokenize(char *buffer, char *delimiter);
 char **_realloc(char **ptr, size_t *size);
 char *new_strtok(char *str, const char *delim);
+void add_key(vars_t *shell_vars);
 
-void (*check_for_builtins(vars_t *vars))(vars_t *vars);
-void new_exit(vars_t *vars);
-void _env(vars_t *vars);
-void new_setenv(vars_t *vars);
-void new_unsetenv(vars_t *vars);
-
-void add_key(vars_t *vars);
 char **find_key(char **env, char *key);
 char *add_value(char *key, char *value);
 int _atoi(char *str);
-
-void check_for_path(vars_t *vars);
-int path_execute(char *command, vars_t *vars);
+/* PATHS*/
+void check_for_path(vars_t *shell_vars);
+int path_execute(char *command, vars_t *shell_vars);
 char *find_path(char **env);
-int execute_cwd(vars_t *vars);
+int execute_cwd(vars_t *shell_vars);
 int check_for_dir(char *str);
-
-void print_error(vars_t *vars, char *msg);
+/*ERRORS*/
+void print_error(vars_t *shell_vars, char *msg);
 void _puts2(char *str);
 char *_uitoa(unsigned int count);
 
