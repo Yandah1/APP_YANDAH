d6d49bd0dd5ba335c51ee5e134ce0466a923996d
Nonkululeko <132392824+Yandah1@users.noreply.github.com>
2023-08-23 02:26:34 +0200

    Update simple_shell.c

--- simple_shell-main/simple_shell.c
+++ simple_shell-main/simple_shell.c
@@ -19,11 +19,10 @@ static void sig_handler(int uuv)
 }
 
 /**
- * main - main function for the shell
- * @argc: number of arguments passed to main
- * @argv: array of arguments passed to main
+ * main - main function for this shell
+ * @argc: number of arguments
+ * @argv: array of arguments
  * @environment: array of environment variables
- *
  * Return: 0 or exit status, or ?
  */
 int main(int argc __attribute__((unused)), char **argv, char **environment)
@@ -32,37 +31,37 @@ int main(int argc __attribute__((unused)), char **argv, char **environment)
 	unsigned int is_pipe = 0, i;
 	vars_t vars = {NULL, NULL, NULL, 0, NULL, 0, NULL};
 
-	vars.argv = argv;
-	vars.env = make_env(environment);
+	shell_vars.argv = argv;
+	shell_vars.env = make_env(environment);
 	signal(SIGINT, sig_handler);
 	if (!isatty(STDIN_FILENO))
 		is_pipe = 1;
 	if (is_pipe == 0)
 		_puts("$ ");
 	sig_flag = 0;
-	while (getline(&(vars.buffer), &len_buffer, stdin) != -1)
+	while (getline(&(shell_vars.buffer), &len_buffer, stdin) != -1)
 	{
 		sig_flag = 1;
-		vars.count++;
-		vars.commands = tokenize(vars.buffer, ";");
-		for (i = 0; vars.commands && vars.commands[i] != NULL; i++)
+		shell_vars.count++;
+		shell_vars.commands = tokenize(shell_vars.buffer, ";");
+		for (i = 0; shell_vars.commands && shell_vars.commands[i] != NULL; i++)
 		{
-			vars.av = tokenize(vars.commands[i], "\n \t\r");
-			if (vars.av && vars.av[0])
-				if (check_for_builtins(&vars) == NULL)
-					check_for_path(&vars);
-			free(vars.av);
+			shell_vars.av = tokenize(shell_vars.commands[i], "\n \t\r");
+			if (shell_vars.av && shell_vars.av[0])
+				if (check_for_builtins(shell_&vars) == NULL)
+					check_for_path(&shell_vars);
+			free(shell_vars.av);
 		}
-		free(vars.buffer);
-		free(vars.commands);
+		free(shell_vars.buffer);
+		free(shell_vars.commands);
 		sig_flag = 0;
 		if (is_pipe == 0)
 			_puts("$ ");
-		vars.buffer = NULL;
+		shell_vars.buffer = NULL;
 	}
 	if (is_pipe == 0)
 		_puts("\n");
-	free_env(vars.env);
-	free(vars.buffer);
-	exit(vars.status);
+	free_env(shell_vars.env);
+	free(shell_vars.buffer);
+	exit(shell_vars.status);
 }
